4624291589181aa83919a1319eb4ac04cb481440
AngelAzrail <androsovdima99@gmail.com>
2022-02-16 17:01:17 +0300

    can choose game as number

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,6 +2,10 @@
 
 import startGame from '../src/index.js';
 import { chooseAGame } from '../src/cli.js';
+import { games } from '../src/enums.js';
 
-const chosenGame = chooseAGame();
+let chosenGame = chooseAGame();
+if (/\d+/.test(chosenGame)) {
+  chosenGame = Object.entries(games).map(([name]) => name)[chosenGame - 1];
+}
 startGame(chosenGame);
